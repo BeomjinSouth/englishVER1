b1d4beef2f06804bc3c854bfc8015866e6cb18d8
BeomjinSouth <159209289+BeomjinSouth@users.noreply.github.com>
2024-07-31 14:21:08 +0900

    Update integral.py

--- integral.py
+++ integral.py
@@ -6,27 +6,6 @@ import matplotlib.pyplot as plt
 def app():
     st.title("적분 계산기")
     
-    def merge_lines(lines):
-        merged = []
-        current_sentence = ""
-        for line in lines:
-            line = line.strip()
-            if line.endswith('.') or line.endswith('?') or line.endswith('!'):
-                current_sentence += " " + line
-                merged.append(current_sentence.strip())
-                current_sentence = ""
-            else:
-                current_sentence += " " + line
-        if current_sentence:
-            merged.append(current_sentence.strip())
-        return merged
-
-    def get_voice(option, idx, gender):
-        voices = {'female': ['alloy', 'fable', 'nova', 'shimmer'], 'male': ['echo', 'onyx']}
-        if option == "random":
-            return np.random.choice(voices[gender])
-        return voices[gender][idx % len(voices[gender])]
-
     def plot_func_and_points(func, x_array, y_array):
         x = sp.symbols('x')
         p = sp.plot(func, (x, -2, float(max(x_array)) + 2), show=False)
@@ -79,20 +58,25 @@ def app():
     x_sup = st.number_input("상한값 (x_sup)을 입력하세요:", value=1.0)
     delta_x = st.number_input("리만 합의 델타 x를 입력하세요:", value=0.1, min_value=0.01)
 
-    x = sp.symbols('x')
-    func = sp.sympify(func_input)
+    # 입력 값 검증
+    if '=' in func_input:
+        st.error("함수를 입력할 때 'y=' 부분을 제거하고 입력하세요.")
+    else:
+        x = sp.symbols('x')
+        func = sp.sympify(func_input)
 
-    if st.button("함수와 점들 그리기"):
-        y_vals = [func.subs(x, val) for val in np.arange(x_inf, x_sup, delta_x)]
-        fig = plot_func_and_points(func, np.arange(x_inf, x_sup, delta_x), y_vals)
-        st.pyplot(fig)
+        if st.button("함수와 점들 그리기"):
+            y_vals = [func.subs(x, val) for val in np.arange(x_inf, x_sup, delta_x)]
+            fig = plot_func_and_points(func, np.arange(x_inf, x_sup, delta_x), y_vals)
+            st.pyplot(fig)
 
-    if st.button("면적 계산하기"):
-        area_result = area(func, x_inf, x_sup, delta_x)
-        st.write(f"{x_inf}에서 {x_sup}까지 곡선 아래의 면적은: {area_result} 입니다.")
+        if st.button("면적 계산하기"):
+            area_result = area(func, x_inf, x_sup, delta_x)
+            st.write(f"{x_inf}에서 {x_sup}까지 곡선 아래의 면적은: {area_result} 입니다.")
 
-    if st.button("리만 합 그리기"):
-        fig, left_riemann_sum, right_riemann_sum = plot_riemann_sums(func, x_inf, x_sup, delta_x)
-        st.pyplot(fig)
-        st.write(f"좌측 리만 합: {left_riemann_sum}")
-        st.write(f"우측 리만 합: {right_riemann_sum}")
+        if st.button("리만 합 그리기"):
+            fig, left_riemann_sum, right_riemann_sum = plot_riemann_sums(func, x_inf, x_sup, delta_x)
+            st.pyplot(fig)
+            st.write(f"좌측 리만 합: {left_riemann_sum}")
+            st.write(f"우측 리만 합: {right_riemann_sum}")
+
