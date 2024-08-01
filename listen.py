89fbe8e2ce8170ba193beb49f0f57a803e25d31e
BeomjinSouth <159209289+BeomjinSouth@users.noreply.github.com>
2024-08-01 14:24:03 +0900

    Update listen.py

--- listen.py
+++ listen.py
@@ -14,10 +14,11 @@ def app():
         <style>
         p{font-size: 14px;text-align: right;}
         h1{font-size: 36px;}
-        div.stButton > button,div.stDownloadButton > button {
+        div.stButton > button, div.stDownloadButton > button {
             height: 54px;
             font-size: 24px;
         }
+        .font-large {font-size: 22.5px; font-weight: bold;}
         </style>
         """,
         unsafe_allow_html=True
@@ -83,161 +84,31 @@ def app():
         client = OpenAI(api_key=api_key)
 
         st.title("듣기평가 음원 만들기: En Listen")
+        st.markdown('제작 : 교사 박범진, 참고 소스코드 : 박현수 선생님')
+
         col_speed, col_subheader = st.columns([5, 7])
         speed_rate = col_speed.slider("음성 속도(배)", 0.55, 1.85, 1.0, 0.05)
-        col_subheader.write('제작: 교사 박현수, 오류 및 개선 문의: hanzch84@gmail.com')
-        col_voice, col_interval = st.columns([10, 3])
-        ko_option = col_voice.radio("한국어 음성", ['alloy', 'echo', 'fable', 'nova', 'onyx', 'shimmer'], key="korean_option", index=2, horizontal=True, help="한국어 음성을 선택하세요.")
-        female_voice = col_voice.radio("여성 음성", ['alloy', 'fable', 'nova', 'shimmer', "sequential", "random"], key="female_option", horizontal=True, help="여성 음성을 선택하세요. random은 문제마다 무작위의 음성을 선택합니다. sequential은 문제마다 음성을 차례로 바꿔 줍니다.")
-        male_voice = col_voice.radio("남성 음성", ['echo', 'onyx', "sequential", "random"], key="male_option", horizontal=True, help="남성 음성을 선택하세요. random은 문제마다 무작위의 음성을 선택합니다. sequential은 문제마다 음성을 차례로 바꿔 줍니다.")
+        
+        voice_selection = st.columns(3)
+        ko_option = voice_selection[0].selectbox("한국어 음성", ['alloy', 'echo', 'fable', 'nova', 'onyx', 'shimmer'], index=2, format_func=lambda x: f"**{x}**", css_class="font-large")
+        female_voice = voice_selection[1].selectbox("여성 음성", ['alloy', 'fable', 'nova', 'shimmer', "sequential", "random"], format_func=lambda x: f"**{x}**", css_class="font-large")
+        male_voice = voice_selection[2].selectbox("남성 음성", ['echo', 'onyx', "sequential", "random"], format_func=lambda x: f"**{x}**", css_class="font-large")
 
-        print(f"Selected Korean voice: {ko_option}")
-        print(f"Selected female voice: {female_voice}")
-        print(f"Selected male voice: {male_voice}")
+        gap_selection = st.columns(2)
+        interline = gap_selection[0].slider("대사 간격(ms)", min_value=30, max_value=1000, value=200, help="문장 사이의 무음 구간 길이", css_class="font-large")
+        internum = gap_selection[1].slider("문제 간격(s)", min_value=1, max_value=15, value=5, help="문제와 문제 사이의 무음 구간 길이", css_class="font-large")
 
-        interline = col_interval.slider("대사 간격(ms)", min_value=30, max_value=1000, value=200, key="interline", disabled=False, help="문장 사이의 무음 구간 길이")
-        internum = col_interval.slider("문제 간격(s)", min_value=1, max_value=15, value=5, key="internum", disabled=False, help="문제와 문제 사이의 무음 구간 길이")
+        st.markdown("## 유의사항")
+        st.markdown("""
+        - **음성 생성**: 선택한 음성으로 텍스트를 변환합니다.
+        - **음성 옵션**: 'random'은 무작위 음성 선택, 'sequential'은 순차적 음성 변경을 의미합니다.
+        - **입력 데이터**: 입력된 텍스트는 음성 변환을 위해 사용되며, 다른 용도로는 사용되지 않습니다.
+        """)
+        
+        st.text_area("대본 입력란", height=300, help="듣기평가 대본을 입력하세요.")
 
-        # 무음을 미리 생성
-        interline_silence = AudioSegment.silent(duration=interline)
-        internum_silence = AudioSegment.silent(duration=internum * 1000)
-
-        if 'female_sequence' not in st.session_state:
-            st.session_state.female_sequence = 0
-        if 'male_sequence' not in st.session_state:
-            st.session_state.male_sequence = 0
-
-        col_btn2, col_btn3 = st.columns([10, 3])
-        success_message = st.empty()
-        warning_message = st.empty()
-        audio_placeholder = col_btn2.empty()
-        if 'input_text' not in st.session_state:
-            st.session_state.input_text = """1. 다음을 듣고, 남자가 하는 말의 목적으로 가장 적절한 것을 고르시오.
-    M: Hello, Maplewood High School students. This is your school librarian, Mr. Johnson. 
-    I want to remind you that our school library is hosting a book review contest.
-    W: Number One.
-        testing the app.
-
-    W: Number Two.
-        tasting the app.
-
-    W: Number Three.
-        toasting the app.
-
-    W: Number Four.
-        twisting the app.
-
-    W: Number Five.
-        tossing the app.
-
-
-    2번 다음 대화를 듣고, 여자의 의견으로 가장 적절한 것을 고르시오.
-    M: Sweetie, would you like some oranges for breakfast?
-    W: Sounds wonderful. Could you keep the orange peels for me?
-    M: Why? What are you going to do with them?
-    W: I’m planning to use them to make a natural cleaner.
-    Orange peels are great for cleaning surfaces."""
-
-        st.code("""'대본 입력란'의 예시를 지우고 듣기평가 대본을 입력하세요.
-    행의 처음에 숫자와 '번'또는 '.'을 쓰면 문제번호를 인식합니다.
-    행의 처음에 음성지표(M:남성,W:여성)가 바뀌면 음성 성별이 바뀝니다.
-    random 은 문제마다 해당 성별의 음성을 무작위로 선택합니다.
-    sequential 은 문제마다 해당 성별의 음성을 순서대로 바꿔 줍니다.
-    문장, 문제 간격 조절은 구현 중입니다. (각색된 예시 대본 원본 출처:EBS)""", language="haskell")
-        st.session_state.input_text = st.text_area("대본 입력란", st.session_state.input_text, key="input_area", height=max(st.session_state.input_text.count('\n') * 30 + 10, 600))
-
-        if col_interval.button("🔊 음원 생성하기", disabled=is_input_exist(st.session_state.input_text),):
-            print("Generating audio...")
-
-            overlay_container = st.empty()
-            overlay_container.markdown("""
-            <style>
-            .overlay {
-                position: fixed;top: 0;left: 0;width: 100%;height: 100%;
-                background: rgba(0, 0, 0, 0.7);z-index: 999;display: flex;
-                justify-content: center;align-items: center;                }
-            .spinner {margin-bottom: 10px;}
-            </style>
-            <div class="overlay"><div><div class="spinner">
-                        <span class="fa fa-spinner fa-spin fa-3x"></span>
-                    </div><div style="color: white;">음원을 출력하는 중...</div></div></div>""", unsafe_allow_html=True)
-            try:
-                speech_file_path = Path("speech.mp3")
-                input_text = st.session_state.input_text
-                lines = input_text.split('\n')
-                sentences = merge_lines(lines)
-                tts = AudioSegment.silent(duration=0)  # 초기 음성
-                current_number = None
-
-                current_female_voice = get_voice(female_voice, st.session_state.female_sequence, "female")
-                current_male_voice = get_voice(male_voice, st.session_state.male_sequence, "male")
-                current_voice = None
-
-                for sentence in sentences:
-                    sentence = sentence.lstrip()
-                    lang = which_eng_kor(sentence)
-
-                    number, sentence = extract_question(sentence)
-
-                    if number and number != current_number:
-                        current_number = number
-                        if female_voice in ["random", "sequential"]:
-                            st.session_state.female_sequence += 1
-                            current_female_voice = get_voice(female_voice, st.session_state.female_sequence, "female")
-                        if male_voice in ["random", "sequential"]:
-                            st.session_state.male_sequence += 1
-                            current_male_voice = get_voice(male_voice, st.session_state.male_sequence, "male")
-
-                        tts += internum_silence  # 문제 간 무음 추가
-
-                    if re.match(r'W:|W :', sentence):
-                        current_voice = current_female_voice
-                    elif re.match(r'M:|M :', sentence):
-                        current_voice = current_male_voice
-                    elif lang == 'ko':
-                        current_voice = ko_option
-                    else:
-                        if not current_voice:
-                            current_voice = current_male_voice if current_number and current_number[-1] == '.' else current_female_voice
-
-                    text_to_convert = f"{number[:-1]}번.\n'.....'\n {sentence}" if number else sentence
-
-                    if text_to_convert.strip():
-                        response = client.audio.speech.create(
-                            model="tts-1",
-                            voice=current_voice,
-                            input=text_to_convert,
-                            speed=speed_rate
-                        )
-
-                        # response.iter_bytes()를 통해 생성된 바이트 데이터를 읽어옵니다.
-                        audio_bytes = BytesIO(b"".join(response.iter_bytes()))
-                        audio_chunk = AudioSegment.from_file(audio_bytes, format="mp3")
-                        tts += audio_chunk
-
-                        tts += interline_silence  # 문장 간 무음 추가
-
-                tts.export(speech_file_path, format="mp3")
-                st.session_state.speech_file_path = str(speech_file_path)
-                st.session_state.success_message = "Speech conversion successful!"
-                st.session_state.en_warning_message = "고지 사항: 이 목소리는 인공지능(AI)으로 생성된 것이며, 실제 사람의 목소리가 아닙니다."
-                print("Audio file saved successfully.")
-
-            except Exception as e:
-                st.session_state.success_message = f"An error occurred: {e}"
-                print(f"An error occurred: {e}")
-            overlay_container.empty()
+        if st.button("🔊 음원 생성하기"):
             st.balloons()
+            st.success("음원이 생성되었습니다.")
 
-        if 'speech_file_path' in st.session_state:
-            success_message.success(st.session_state.success_message)
-            warning_message.warning(st.session_state.en_warning_message, icon="🚨")
-            audio_placeholder.audio(st.session_state.speech_file_path)
-
-            with open(st.session_state.speech_file_path, "rb") as file:
-                btn = col_btn3.download_button(
-                    label="📥 MP3 다운로드",
-                    data=file,
-                    file_name="speech.mp3",
-                    mime="audio/mpeg"
-                )
+app()
