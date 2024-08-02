afca6252a4a05939d63fa2e0b3975edb0610a465
BeomjinSouth <159209289+BeomjinSouth@users.noreply.github.com>
2024-08-02 10:38:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import listen
 import integral
 import integraledit  # integraledit 모듈 불러오기
+import pdfchatbot
 
 st.set_page_config(
     page_title="Multi App",
@@ -13,7 +14,7 @@ st.set_page_config(
 PAGES = {
     "듣기평가 음원 만들기": listen,
     "Integral": integral,
-    "적분 계산기": integraledit  # 새로운 페이지 추가
+    "적분 계산기": integraledit,  # 새로운 페이지 추가
     "PDF 챗봇": pdfchatbot
 }
 
