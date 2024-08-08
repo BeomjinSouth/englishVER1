8d02bfaf274321b321ed2684d813460ea9692f38
BeomjinSouth <159209289+BeomjinSouth@users.noreply.github.com>
2024-08-08 10:43:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import listen
 import integral
 import integraledit  # integraledit 모듈 불러오기
 import pdfchatbot
+import triangle  # triangle 모듈 불러오기
 
 st.set_page_config(
     page_title="Multi App",
@@ -15,7 +16,8 @@ PAGES = {
     "듣기평가 음원 만들기": listen,
     "Integral": integral,
     "적분 계산기": integraledit,  # 새로운 페이지 추가
-    "PDF 챗봇": pdfchatbot
+    "PDF 챗봇": pdfchatbot,
+    "삼각형 만들기": triangle  # 새로운 페이지 추가
 }
 
 st.sidebar.title("Navigation")
